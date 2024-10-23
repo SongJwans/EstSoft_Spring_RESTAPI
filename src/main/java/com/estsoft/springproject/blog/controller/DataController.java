779daaef332f1620081fefba56556d743e1c0ad6
SongJwans <yhh5916@naver.com>
2024-10-23 22:10:08 +0900

    Data 추가를 위한 Controller 추가

--- src/main/java/com/estsoft/springproject/blog/controller/DataController.java
+++ src/main/java/com/estsoft/springproject/blog/controller/DataController.java
@@ -1,2 +1,28 @@
-package com.estsoft.springproject.blog.controller;public class DataController {
+package com.estsoft.springproject.blog.controller;
+
+import com.estsoft.springproject.blog.domain.dto.ArticleDTO;
+import com.estsoft.springproject.blog.domain.dto.CommentDTO;
+import com.estsoft.springproject.blog.service.DataImportService;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+@RequiredArgsConstructor
+public class DataController {
+    private final DataImportService dataImportService;
+
+    @GetMapping("/import-articles")
+    public ResponseEntity<List<ArticleDTO>> importArticles() {
+        return ResponseEntity.ok(dataImportService.importArticles().stream().map(ArticleDTO::new).toList());
+    }
+
+    @GetMapping("/import-comments")
+    public ResponseEntity<List<CommentDTO>> importComments() {
+        return ResponseEntity.ok(dataImportService.importComments().stream().map(CommentDTO::new).toList());
+    }
 }
