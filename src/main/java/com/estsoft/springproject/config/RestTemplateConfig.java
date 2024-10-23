7ce91f8ecd682a3b87f8952adfe2f8e8faa479e3
SongJwans <yhh5916@naver.com>
2024-10-23 22:11:13 +0900

    RestTemplateConfig 추가

--- src/main/java/com/estsoft/springproject/config/RestTemplateConfig.java
+++ src/main/java/com/estsoft/springproject/config/RestTemplateConfig.java
@@ -1,2 +1,13 @@
-package com.estsoft.springproject.config;public class RestTemplateConfig {
+package com.estsoft.springproject.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+@Configuration
+public class RestTemplateConfig {
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
 }
